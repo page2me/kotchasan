35057cdfab8653560e18e0ea0f76e48ac96ec7ef
poo <poo@goragod>
2016-08-08 09:49:54 +0700

    init git

---
+++ projects/recordset/datas/logs/error_log.php
@@ -0,0 +1,7 @@
+<?php exit() ?>
+[2016-07-29 10:25:03] ERROR: connect : <em>SQLSTATE[HY000] [1045] Access denied for user 'plus'@'localhost' (using password: YES)</em> in <b>/mnt/Server/htdocs/kotchasan/Kotchasan/Database.php</b> on line <b>80</b> 
+[2016-07-29 10:26:27] ERROR: connect : <em>SQLSTATE[HY000] [1045] Access denied for user 'plus'@'localhost' (using password: YES)</em> in <b>/mnt/Server/htdocs/kotchasan/Kotchasan/Database.php</b> on line <b>80</b> 
+[2016-07-29 10:26:28] ERROR: connect : <em>SQLSTATE[HY000] [1045] Access denied for user 'plus'@'localhost' (using password: YES)</em> in <b>/mnt/Server/htdocs/kotchasan/Kotchasan/Database.php</b> on line <b>80</b> 
+[2016-07-29 10:26:29] ERROR: connect : <em>SQLSTATE[HY000] [1045] Access denied for user 'plus'@'localhost' (using password: YES)</em> in <b>/mnt/Server/htdocs/kotchasan/Kotchasan/Database.php</b> on line <b>80</b> 
+[2016-07-29 10:31:11] ERROR: connect : <em>SQLSTATE[HY000] [1044] Access denied for user 'plus'@'localhost' to database 'gcms_plus'</em> in <b>/mnt/Server/htdocs/kotchasan/Kotchasan/Database.php</b> on line <b>80</b> 
+[2016-07-29 10:31:12] ERROR: connect : <em>SQLSTATE[HY000] [1044] Access denied for user 'plus'@'localhost' to database 'gcms_plus'</em> in <b>/mnt/Server/htdocs/kotchasan/Kotchasan/Database.php</b> on line <b>80</b> 
